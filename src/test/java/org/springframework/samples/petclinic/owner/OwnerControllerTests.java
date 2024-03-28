8c7d4952bc3e8a4828b643a40a27af38c01397ea
BrandonMcA <bmcardle541@gmail.com>
2024-03-28 14:07:37 +0000

    Intentionally failed test

--- src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerControllerTests.java
@@ -70,7 +70,7 @@ class OwnerControllerTests {
 	private Owner george() {
 		Owner george = new Owner();
 		george.setId(TEST_OWNER_ID);
-		george.setFirstName("George");
+		george.setFirstName("eorge");
 		george.setLastName("Franklin");
 		george.setAddress("110 W. Liberty St.");
 		george.setCity("Madison");
